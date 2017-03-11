76ea3631f104512e0a877bdffd5efeba2612fe7a
Eric P. Katz <ericpkatz@gmail.com>
2017-03-11 16:05:57 -0500

    commit

--- public/app.js
+++ public/app.js
@@ -12,41 +12,7 @@ var state = {
       store: stores[0]
     }
   ]
-}
-
-function DayView(containerId, day, onRemoveOffice, onRemoveStore){
-  var container = $(containerId);
-  container.empty();
-  var officeVisit = day.office;
-  var storeVisit = day.store;
-  var storeItem = storeVisit ? `<li class='list-group-item store'>Store: ${storeVisit.name}</li>`: '';
-  var officeItem = officeVisit ? `<li class='list-group-item office'>Office: ${officeVisit.name}</li>`: '';
-  var ul = $(`<ul class='list-group'>${storeItem}${officeItem}</ul>`);
-  ul.on('click', 'li.store', function(){
-    onRemoveStore();
-  });
-  ul.on('click', 'li.office', function(){
-    onRemoveOffice();
-  });
-  container.append(ul);
-}
-
-function DayPicker(containerId, days, idx, onSelectDay){
-  var container = $(containerId);
-  container.empty();
-  var lis = days.map(function(day, _idx){
-    if(idx === _idx){
-      return `<li class='active'><a>${_idx + 1}</a></li>`;
-    }
-    return `<li><a>${_idx + 1}</a></li>`;
-  });
-  var ul = $(`<ul class='nav nav-tabs'>${lis.join('')}</ul>`);
-  ul.on('click', 'li', function(){
-    var index = $(this).index();
-    onSelectDay(index);
-  });
-  container.append(ul);
-}
+};
 
 function onSelectDay(idx){
   state.idx = idx;
@@ -79,37 +45,41 @@ function onAddDay(){
   renderDayPicker();
 }
 
+function onRemoveDay(){
+  state.days.splice(state.days.index, 1);
+  state.idx = 0;
+  renderDayPicker();
+}
+
+var stateChangeHandlers = {
+  onSelectDay,
+  onAddOffice,
+  onAddStore,
+  onRemoveOffice,
+  onRemoveStore,
+  onAddDay,
+  onRemoveDay
+};
+
 function renderDayView(){
-  DayView('#dayView', state.days[state.idx], onRemoveOffice, onRemoveStore);
+  DayView('#dayView', state.days[state.idx], stateChangeHandlers.onRemoveOffice, stateChangeHandlers.onRemoveStore);
 }
 
 function renderDayPicker(){
-  DayPicker('#dayPicker', state.days, state.idx, onSelectDay);
+  DayPicker(
+      '#dayPicker',
+      state.days,
+      state.idx,
+      stateChangeHandlers.onSelectDay,
+      stateChangeHandlers.onAddDay,
+      stateChangeHandlers.onRemoveDay
+  );
   renderDayView();
 }
 
-function VisitPickers(containerId, onAddOffice, onAddStore){
-  var container = $(containerId);
-  $('.btn', container).on('click', function(ev){
-    ev.preventDefault();
-    var form = $(this).parents('form');
-    var index = form.index();
-    var list = index === 0 ? offices : stores;
-    var mode = index === 0 ? 'offices' : 'stores';
-    var select = $('select', form);
-    var id = select.val();
-    var item = list.filter(function(item){
-      return item.id === id*1;
-    })[0];
-    if(mode === 'offices'){
-      onAddOffice(item);
-    }
-    if(mode === 'stores'){
-      onAddStore(item);
-    }
-  });
-
+function init(){
+  renderDayPicker();
+  VisitPickers('#visitPickers', stateChangeHandlers.onAddOffice, stateChangeHandlers.onAddStore);
 }
 
-renderDayPicker();
-VisitPickers('#visitPickers', onAddOffice, onAddStore);
+init();
